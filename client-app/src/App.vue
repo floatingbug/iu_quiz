d7f01bde5406b5867a40ead33bbeb3100b3c6568
floatingbug <sieber.thomas@yahoo.com>
2023-05-25 10:17:12 +0200

    client-app merged into main

---
+++ client-app/src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import Navbar from './components/Navbar.vue';
+</script>
+
+<template>
+<Navbar></Navbar>
+
+<RouterView />
+</template>
+
+<style scoped>
+
+</style>
---
+++ client-app/src/router/index.js
@@ -0,0 +1,37 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import Login from '../views/Login.vue';
+import Lobby from '../views/Lobby.vue';
+import Scoreboard from '../views/Scoreboard.vue';
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView
+    },
+      
+    {
+      path: '/login-user',
+      name: 'login-user',
+      component: Login
+    },
+
+    {
+        path: '/lobby',
+        name: 'lobby',
+        component: Lobby 
+    },
+
+    {
+        path: '/scoreboard',
+          name: 'scoreboard',
+          component: Scoreboard
+    },
+
+  ]
+})
+
+export default router
