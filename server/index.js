e828e7546f4ab8603b538b5ab668b67ad3165701
Pierre <pize92@gmail.com>
2023-06-09 20:10:03 +0200

    test

--- server/index.js
+++ server/index.js
@@ -2,8 +2,9 @@ require('dotenv').config();
 const http = require('http');
 const {createApi} = require('./lib/createApi');
 const {MongoStore} = require('./lib/store/MongoStore');
-const store = new MongoStore(process.env.DB_URL);
-store.initialize();
+//const store = new MongoStore(process.env.DB_URL);
+//store.initialize();
+const store = {};
 const {lobbyStore} = require('./lib/store/lobbyStore');
 const {gameLoop} = require('./lib/gameLoop');
 
