5510f7da4b5a42e1c783ddc22d74e7aafa5cb3dc
floatingbug <sieber.thomas@yahoo.com>
2023-06-12 19:32:22 +0200

    InMemoryStore implemented

--- server/index.js
+++ server/index.js
@@ -1,9 +1,10 @@
 require('dotenv').config();
+const {InMemoryStore} = require('./lib/store/InMemoryStore');
 const http = require('http');
 const {createApi} = require('./lib/createApi');
-const {MongoStore} = require('./lib/store/MongoStore');
-const store = new MongoStore(process.env.DB_URL);
-store.initialize();
+//const {MongoStore} = require('./lib/store/MongoStore');
+//const store = new MongoStore(process.env.DB_URL);
+const store = new InMemoryStore();
 const {lobbyStore} = require('./lib/store/lobbyStore');
 const {gameLoop} = require('./lib/gameLoop');
 
