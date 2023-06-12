24b8d28a9ff1a73e9e2ccec34bb0350008498efb
Saitamasama <125801620+Saitamasama@users.noreply.github.com>
2023-06-12 20:53:32 +0200

    Todo's in server/createLobby und src/components/createLobby bearbeitet, component/createLobby bitte kontrollieren (siehe Kommentar)

--- server/lib/routes/createLobby.js
+++ server/lib/routes/createLobby.js
@@ -2,15 +2,30 @@ function createLobby({store, lobbyStore}){
     return async (req, res)=>{
         const lobby = req.body;
 
-        //todo: check also if the follow popperties are awailable: time, theme, numberQuestions.
+
         if(!lobby || !lobby.groupName || !lobby.lobbyId || !lobby.players || !lobby.gameMode){
             res.json({code: 1, msg: "some options are missed"})
             return
         }
-
-        //todo: check if time is 30, 50, or 60 secs.
-
-        
+        // abgeschlossene todo: kontrolle ausstehend
+        if(!lobby.time){
+            res.json[{code: 1, msg: "time is missing"}]
+            return
+        }
+        if(!lobby.theme){
+            res.json[{code: 1, msg: "theme is missing"}]
+            return
+        }
+        if(!lobby.numberQuestions){
+            res.json[{code: 1, msg: "numberQuesetions is missing"}]
+            return
+        }
+        if(lobby.time !== 35 && lobby.time !== 45 && lobby.time !== 60){
+            res.json[{ code: 1, msg: "time is not 30, 45 or 60"}]
+            return
+        }
+        // bis hier hin
+       
         const result = lobbyStore.addLobby(lobby);
         console.log("-------------------")
         console.log(result)
