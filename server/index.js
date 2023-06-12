2715b8fc081233f2611ec74949795a1e77bb29cc
Pierre <pize92@gmail.com>
2023-06-12 19:34:20 +0200

    git ignore angepasst

--- server/index.js
+++ server/index.js
@@ -1,9 +1,10 @@
 require('dotenv').config();
 const http = require('http');
 const {createApi} = require('./lib/createApi');
-const {MongoStore} = require('./lib/store/MongoStore');
-const store = new MongoStore(process.env.DB_URL);
-store.initialize();
+//const {MongoStore} = require('./lib/store/MongoStore');
+//const store = new MongoStore(process.env.DB_URL);
+//store.initialize();
+const store = {};
 const {lobbyStore} = require('./lib/store/lobbyStore');
 const {gameLoop} = require('./lib/gameLoop');
 
