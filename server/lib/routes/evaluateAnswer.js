f3e4d9f3149562c6f8230930c3f1e3a4ff9c9027
Saitamasama <125801620+Saitamasama@users.noreply.github.com>
2023-06-13 20:54:37 +0200

    Todo in Server/evaluateAnswer angefangen aber nicht richtig/abgeschlossen

--- server/lib/routes/evaluateAnswer.js
+++ server/lib/routes/evaluateAnswer.js
@@ -1,13 +1,30 @@
 function evaluateAnswer({store, lobbyStore}){
-    return (req, res)=>{
-        //todo: check if all needed propperties are in payload.
-
+    return (req, res)=>{  
         const playerName = req.body.playerName;
         const lobbyId = req.body.lobbyId;
         const questionId = req.body.questionId;
+        //selectedAnswer existiert bisher nicht !!!
+        const selectedAnswer = req.body.selectedAnswer;
 
-        //evaluate if answer is true or false.
+        //todo umgesetzt, kontrolle ausstehend
+        //ursprünglich todo: check if all needed propperties are in payload.
+        if(!playerName || !lobbyId || !questionId || !selectedAnswer){
+            res.json({code: 1, msg: "some Propperties are missing"})
+            return
+        }
         
+       
+        //todo: evaluate if answer is true or false.
+        //Ich habe mich hier mal dran Versucht, glaube aber leider das ich hier gedanklich einen Fehler habe. Außerdem ist der Zugriff auf rightAnswer falsch,
+        //hier müsste ja eigentlich noch das theme Array durchlaufen werden und die questionId abgeglichen werden. Da ich aber glaube das die funktion grundsätzlich
+        //anders umgestezt werden müsste, habe ich hier nicht weiter gemacht. Ihr düft mir hier gerne helfen oder den todo selbst übernehmen. (Daniel)
+        if(selectedAnswer == lobbyStore.rightAnswer){
+            res.json({code: 0, msg: "right Answer"})
+            return
+        } else {
+            res.json({code: 0, msg: "wrong Answer"})
+            return
+        }
     }
 }
 
