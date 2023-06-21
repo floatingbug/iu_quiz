6b849d91f8a7732a4a5a83ca4b9637eec929a6fd
floatingbug <sieber.thomas@yahoo.com>
2023-06-21 12:17:38 +0200

    iu_quiz v1

--- client-app/src/renderlesComponents/ApiCall.vue
+++ client-app/src/renderlesComponents/ApiCall.vue
@@ -2,7 +2,7 @@
 import axios from 'axios'
 const API_URL_LOCAL = "http://localhost:8000";
 const API_URL_Server = 'http://212.71.252.220:8000';
-const API_URL = API_URL_LOCAL;
+const API_URL = API_URL_Server;
 
 async function call({ method, url, data }, fn) {
     const result = await axios({
