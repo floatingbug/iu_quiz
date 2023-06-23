9e1952a484d8fbe0b4ffb3ee0fe1b3bd21bab060
Saitamasama <125801620+Saitamasama@users.noreply.github.com>
2023-06-23 19:15:37 +0200

    Änderung an Fragen einsenden im User login modus

--- client-app/src/views/FragenEinsendenView.vue
+++ client-app/src/views/FragenEinsendenView.vue
@@ -3,31 +3,50 @@
 <template>
     <div class="container">
         <div class="config-question">
-            <label for=""></label>
-            <input type="" />
-            <label for=""></label>
-            <input type="" />
-            <label for=""></label>
-            <input type="" />
-            <label for=""></label>
-            <input type="" />
-            <label for=""></label>
-            <input type="" />
-            <label for=""></label>
-            <input type="" />
-            <button>Einsenden</button>
+            <label for="theme">Themenbereich:</label>
+            <input type="theme" />
+            <label for="question">Frage:</label>
+            <input type="question" />
+            <label for="answer1">Antwort 1:</label>
+            <input type="answer1" />
+            <label for="answer2">Antwort 2:</label>
+            <input type="answer2" />
+            <label for="answer3">Antwort 3:</label>
+            <input type="answer3" />
+            <label for="answer4">Antwort 4:</label>
+            <input type="answer4" />
+            <div class="button">
+                <button>Einsenden</button>
+            </div>
         </div>
     </div>
 </template>
 
 <style scoped>
 .container {
-    height: 50vh;
     display: flex;
+    height: 30vh;
+    display: flex;
+    color: black;
+    align-items: center;
+    justify-content: center;
+    font-weight: 700;
+    font-size: larger;
+    margin-top: 5vw;
 }
 
 .config-question {
     display: flex;
     flex-direction: column;
+    width: 40%;
+    
+}
+.button{
+    margin-top: 2vw;
+    margin-left: auto;
+}
+
+.config-question input {
+    height: 1.8vw;
 }
 </style>
